d48715df4cdbfb1197f9d2da3ea29658f0ce40c0
Uday Bari <udaybari2003@gmail.com>
2026-04-14 20:05:52 +0530

    added peojects

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ import logo from "../assets/Uday.png";
 import { FaInstagram, FaMoon, FaSun } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
-import file from "../assets/UdayBariResume.pdf";
+import file from "../assets/Resume-Uday-Bari-March.pdf";
 
 const Navbar = ({ setTheme, Theme }) => {
   const [Icon, setIcon] = useState(<FaMoon />);
@@ -24,26 +24,40 @@ const Navbar = ({ setTheme, Theme }) => {
         <img src={logo} alt="logo" />
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <a target="_blank" href={file} download={file} className="text-sm bg-neutral-800 px-2 py-1 rounded text-white dark:text-cyan-700 hover:text-black hover:bg-blue-500">
+        <a
+          target="_blank"
+          href={file}
+          download={file}
+          className="text-sm bg-neutral-800 px-2 py-1 rounded text-white dark:text-cyan-700 hover:text-black hover:bg-blue-500">
           <button>Resume</button>
         </a>
-        <a target="_blank" className="text-sm text-white bg-neutral-800 flex justify-center items-center p-2 rounded-full">
+        <a
+          target="_blank"
+          className="text-sm text-white bg-neutral-800 flex justify-center items-center p-2 rounded-full">
           <button
             onClick={() => {
               changeTheme();
-            }}
-          >
+            }}>
             {Icon}
           </button>
         </a>
 
-        <a className="hover:cursor-pointer hover:text-blue-500 " href="https://www.linkedin.com/in/uday-bari-003530221/" target="_blank">
+        <a
+          className="hover:cursor-pointer hover:text-blue-500 "
+          href="https://www.linkedin.com/in/uday-bari-003530221/"
+          target="_blank">
           <FaLinkedin />
         </a>
-        <a className="hover:cursor-pointer rounded-full hover:bg-black hover:text-white" href="https://github.com/UdayBari3421" target="_blank">
+        <a
+          className="hover:cursor-pointer rounded-full hover:bg-black hover:text-white"
+          href="https://github.com/UdayBari3421"
+          target="_blank">
           <FaGithub />
         </a>
-        <a className="hover:cursor-pointer hover:text-pink-500" href="https://t.me/udaybari" target="_blank">
+        <a
+          className="hover:cursor-pointer hover:text-pink-500"
+          href="https://t.me/udaybari"
+          target="_blank">
           <FaInstagram />
         </a>
       </div>
--- src/constants/index.js
+++ src/constants/index.js
@@ -5,6 +5,9 @@ import giftShop from "../assets/projects/giftShop.png";
 import GoogleSheet from "../assets/projects/GoogleSheet.png";
 import chatApp from "../assets/projects/chatApp.png";
 import Cookpal from "../assets/projects/Cookpal.png";
+import ecomWebApp from "../assets/projects/ecomWebApp.png";
+import FT from "../assets/projects/FT.png";
+import bloggingPlatform from "../assets/projects/bloggingPlatform.png";
 
 export const HERO_CONTENT = `👋 Hi there! I'm a passionate and dedicated MERN Stack Developer, fresh out of my studies and eager to dive into the world of web development. With a strong foundation in MongoDB, Express.js, React, and Node.js, I've built a variety of dynamic and responsive web applications that showcase my skills and creativity. Explore my projects to see how I bring ideas to life and stay tuned as I continue to learn and grow in this exciting field. Let's build something amazing together!`;
 
@@ -21,14 +24,6 @@ export const PROJECTS = [
     technologies: ["HTML", "CSS", "JavaScript", "Node.js", "Socket.io", "Express"],
     github: "https://github.com/UdayBari3421/ChatApp",
     livelink: "https://chatapp-ev6c.onrender.com",
-  },
-  {
-    title: "Todo App",
-    image: todoAppimg,
-    description: "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "JavaScript", "Node.js", "MongoDB", "Express"],
-    github: "https://github.com/UdayBari3421/express-todo-app",
-    livelink: "https://express-todo-app-mhn6.onrender.com",
   },
   {
     title: "Wheater App",
@@ -57,6 +52,36 @@ export const PROJECTS = [
     livelink: "https://udaybari3421.github.io/F2-Contest1_October",
   },
   {
+    title: "E-Commerce Website",
+    image: giftShop,
+    description: "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "JavaScript", "PHP", "PHPmyAdmin"],
+    github: "https://github.com/UdayBari3421/BCA_Project",
+  },
+  {
+    title: "E-Commerce Website",
+    image: ecomWebApp,
+    description: "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "JavaScript", "Node.js", "MongoDB", "Express"],
+    github: "https://github.com/UdayBari3421/EcomWebApp.git",
+    livelink: "https://ecom-web-app-frontend.vercel.app/",
+  },
+  {
+    title: "Finance Tracker",
+    image: FT,
+    description: "A finance tracker application that allows users to track their income and expenses, set budgets, and generate financial reports. Users can categorize their transactions, view spending patterns, and gain insights into their financial health.",
+    technologies: ["HTML", "CSS", "JavaScript", "Node.js", "Chart.js", "Express", "Firebase"],
+    github: "https://github.com/UdayBari3421/Finance-Tracker",
+    livelink: "https://finance-tracker-frontend.vercel.app/",
+  },
+  {
+    title: "Blogging Platform",
+    image: bloggingPlatform,
+    description: "A simple blogging platform where users can create, edit, and publish blog posts. It includes features like user authentication, comment sections, and a responsive design.",
+    technologies: ["HTML", "CSS", "JavaScript", "Node.js", "Express", "MongoDB"],
+    github: "https://github.com/UdayBari3421/Blogging-Platform",
+    livelink: "https://udaybari3421.github.io/Blogging-Platform/"
+  }, {
     title: "Cookpal Food Delivery",
     image: Cookpal,
     description:
@@ -64,13 +89,6 @@ export const PROJECTS = [
     technologies: ["HTML", "CSS", "React", "Bootstrap"],
     github: "https://github.com/UdayBari3421/CookPalFood",
     livelink: "https://udaybari3421.github.io/CookPalFood/",
-  },
-  {
-    title: "E-Commerce Website",
-    image: giftShop,
-    description: "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "JavaScript", "PHP", "PHPmyAdmin"],
-    github: "https://github.com/UdayBari3421/BCA_Project",
   },
 ];
 
